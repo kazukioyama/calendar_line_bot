e01c2a654993e2082d8bdb8cc1948fcf53053d33
Kazuki Oyama <kazuki.oy@gmail.com>
2024-07-17 23:36:52 +0900

    Line::MessageProcesserに置き換えた際のデバックのチェック

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -13,40 +13,25 @@ class WebhookController < ApplicationController
       end
     rescue Line::InvalidSignatureError => e
       render json: { message: 'Invalid signature error' }, status: :unauthorized
-    rescue => e
-      puts e
-      render json: { message: 'Internal Server error' }, status: :internal_server_error
     end
 
     # リクエストボディをパースする
     events = bot.parse_events_from(body)
+
     line_user_id = events[0]['source']['userId']
     user = User.find_or_create_by!(line_user_id: line_user_id)
 
-    # 処理中のメッセージを返す
-    Line::SaveSentMessage.new(admin, user).call_with_text(text: "Processing...")
+    # # 処理中のメッセージを返す
+    # Line::SaveSentMessage.new(admin, user).call_with_text(text: "Processing...")
 
     events.each do |event|
       case event
       when Line::Bot::Event::Message
-        begin
-          Line::SaveReceivedMessage.new(admin, user).call(event) # 受け取ったメッセージをDBに保存
-        rescue => e
-          puts "An unexpected error occurred: #{e.message}"
-          Line::SaveSentMessage.new(admin, user).call_with_text(
-            text: "Sorry, Message creation failed due to an unexpected error."
-          )
-          return
-        end
-        Line::MessageProcesser.new(admin, user, event)
+        Line::SaveReceivedMessage.new(admin, user).call(event) # 受け取ったメッセージをDBに保存
+        Line::MessageProcesser.new(admin, user, event).handle_message # 受け取ったメッセージの文字によって別々の処理を行う
       end
     end
 
     render plain: 'success!', status: :ok
-  end
-
-  private
-  def auth_url(user)
-    auth_google_oauth_index_url(user_id: user.id)
   end
 end
