9bbc741449714f6f9509c4be596b4fa69f9738cf
Kazuki Oyama <kazuki.oy@gmail.com>
2024-07-17 13:43:31 +0900

    20240717

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -6,81 +6,104 @@ class WebhookController < ApplicationController
 
     begin
       admin = Admin.find(params[:admin_id])
-      bot = Line::Api::Bot.new(admin)
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { message: "Admin not found: #{e.message}" }, status: :not_found
+    rescue => e
+      puts e
+      render json: { message: 'Internal Server error' }, status: :internal_server_error
+    end
+
+    bot = Line::Api::Bot.new(admin)
+
+    begin
       # 署名の検証（LINEプラットフォームからのリクエストか判定するため）
       unless bot.validate_signature?(body, request.env['HTTP_X_LINE_SIGNATURE'])
         raise Line::InvalidSignatureError
       end
-
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { message: "Admin not found: #{e.message}" }, status: :not_found
     rescue Line::InvalidSignatureError => e
       render json: { message: 'Invalid signature error' }, status: :unauthorized
     rescue => e
-      puts "An Error occurred!!!"
       puts e
+      render json: { message: 'Internal Server error' }, status: :internal_server_error
+    end
+
+    # リクエストボディをパースする
+    events = bot.parse_events_from(body)
+
+    line_user_id = events[0]['source']['userId']
+    # 処理中のメッセージを返す
+    Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: "Processing...")
+
+    begin
+      user = User.find_or_create_by!(line_user_id: line_user_id)
+    rescue ActiveRecord::RecordInvalid => e
+      puts "User creation failed: #{e.message}"
       Line::SaveSentMessage.new(admin).call_with_text(
         line_user_id: line_user_id,
-        text: "Sorry, an error occurred on the server."
+        text: "Sorry, User creation failed."
       )
     end
 
-    events = bot.parse_events_from(body)
     events.each do |event|
       case event
       when Line::Bot::Event::Message
-        line_user_id = event['source']['userId']
-        puts "line_user_id"
-        puts line_user_id
-        Line::SaveReceivedMessage.new(admin).call(event)
-        Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: "Processing...")
         begin
-          user = User.find_by(line_user_id: line_user_id)
-          # user_id = user.id
-        rescue NoMethodError => e
+          Line::SaveReceivedMessage.new(admin).call(event) # 受け取ったメッセージをDBに保存
+        rescue ActiveRecord::RecordInvalid => e
+          puts "User creation failed: #{e.message}"
           Line::SaveSentMessage.new(admin).call_with_text(
             line_user_id: line_user_id,
-            text: "LINEユーザIDが見つかりません。ログインしてください。"
+            text: "Sorry, User creation failed."
+          )
+        rescue => e
+          puts "An unexpected error occurred: #{e.message}"
+          Line::SaveSentMessage.new(admin).call_with_text(
+            line_user_id: line_user_id,
+            text: "Sorry, Message creation failed due to an unexpected error."
           )
         end
-        if user&.google_access_token&.nil?
+        begin
+          # 受け取ったメッセージ内容によって処理を分岐
+          case event["message"]["text"]
+          when "Hello" # 初期セッティング
+            Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: "Hi! Thank you for using me.")
+            Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: "Please click the following URL to authenticate your Google account.\n\n#{auth_url(user)}")
+          when "Get Upcoming Event"
+            if user.google_access_token.nil?
+              Line::SaveSentMessage.new(admin).call_with_text(
+                line_user_id: line_user_id,
+                text: "Please click the following URL to authenticate your Google account.\n\n#{auth_url(user)}"
+              )
+            end
+            events_list = Calendar::GoogleCalendar.new(user).get_events(1)
+            events_list[:summary].each do |value|
+              Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: value)
+            end
+          when "Get 10 Events"
+            if user.google_access_token.nil?
+              Line::SaveSentMessage.new(admin).call_with_text(
+                line_user_id: line_user_id,
+                text: "Please click the following URL to authenticate your Google account.\n\n#{auth_url(user)}"
+              )
+            end
+            events_list = Calendar::GoogleCalendar.new(user).get_events(10)
+            events_list[:summary].each do |value|
+              Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: value)
+            end
+          when "Switch Between Google Accounts"
+            Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: "Please click the following URL to authenticate your Google account.\n\n#{auth_url(user)}")
+          else
+            Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: "Please enter the correct value.")
+          end
+        rescue => e
+          3.times do
+            puts "An Error occurred!!!"
+          end
+          puts e
           Line::SaveSentMessage.new(admin).call_with_text(
             line_user_id: line_user_id,
-            text: "Please click the following URL to authenticate your Google account.\n\n#{auth_url(user)}"
+            text: "Sorry, an error occurred on the server.\nPlease contact the system administrator or click the following URL to authenticate your Google account again.\n\n#{auth_url(user)}"
           )
-        else
-          begin
-            case event["message"]["text"]
-            when "Get Upcoming Event"
-              events_list = Calendar::GoogleCalendar.new(user).get_events(1)
-              events_list[:summary].each do |value|
-                Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: value)
-              end
-            when "Get 10 Events"
-              events_list = Calendar::GoogleCalendar.new(user).get_events(10)
-              events_list[:summary].each do |value|
-                Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: value)
-              end
-            when "Switch Between Google Accounts"
-              Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: "Please click the following URL to authenticate your Google account.\n\n#{auth_url(user)}")
-            when "Hello"
-              Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: "Hi! Thank you for using me.")
-              Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: "Please click the following URL to authenticate your Google account.\n\n#{auth_url(user)}")
-            else
-              Line::SaveSentMessage.new(admin).call_with_text(line_user_id: line_user_id, text: "Please enter the correct value.")
-            end
-          rescue => e
-            3.times do
-              puts "An Error occurred!!!"
-            end
-            puts e
-            Line::SaveSentMessage.new(admin).call_with_text(
-              line_user_id: line_user_id,
-              text: "Sorry, an error occurred on the server.\nPlease contact the system administrator or click the following URL to authenticate your Google account again.\n\n#{auth_url(user)}"
-            )
-          end
-        # case event.type
-        # when Line::Bot::Event::MessageType::Text
         end
       end
     end
@@ -90,6 +113,6 @@ class WebhookController < ApplicationController
 
   private
   def auth_url(user)
-    return "https://#{ENV['HOST']}/calendar/auth/#{user.id}"
+    "https://#{ENV['HOST']}/calendar/auth/#{user.id}"
   end
 end
--- app/services/line/save_received_message.rb
+++ app/services/line/save_received_message.rb
@@ -2,12 +2,12 @@ module Line
   class SaveReceivedMessage
     def initialize(admin)
       @admin = admin
+      @user = user
     end
 
     def call(event)
-      user = User.find_by(line_user_id: event['source']['userId'])
       resource = MessageText.new(content: event.message['text'])
-      Message.create!(sendable: user, receivable: @admin, resource: resource) if user.present?
+      Message.create!(sendable: @user, receivable: @admin, resource: resource)
     end
   end
 end
