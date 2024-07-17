ca37cd095d83853613729c754a6370a579fad7ab
Kazuki Oyama <kazuki.oy@gmail.com>
2024-07-17 23:06:59 +0900

    controllerの一部をLine::MessageProcesserに置き換え

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -3,16 +3,7 @@ class WebhookController < ApplicationController
 
   def create
     body = request.body.read #LINE側から送信されたリクエストボディ
-
-    begin
-      admin = Admin.find(params[:admin_id])
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { message: "Admin not found: #{e.message}" }, status: :not_found
-    rescue => e
-      puts e
-      render json: { message: 'Internal Server error' }, status: :internal_server_error
-    end
-
+    admin = Admin.find(params[:admin_id])
     bot = Line::Api::Bot.new(admin)
 
     begin
@@ -29,15 +20,8 @@ class WebhookController < ApplicationController
 
     # リクエストボディをパースする
     events = bot.parse_events_from(body)
-
     line_user_id = events[0]['source']['userId']
-
-    begin
-      user = User.find_or_create_by!(line_user_id: line_user_id)
-    rescue ActiveRecord::RecordInvalid => e
-      puts "User creation failed: #{e.message}"
-      render json: { message: 'Internal Server error' }, status: :internal_server_error
-    end
+    user = User.find_or_create_by!(line_user_id: line_user_id)
 
     # 処理中のメッセージを返す
     Line::SaveSentMessage.new(admin, user).call_with_text(text: "Processing...")
@@ -47,12 +31,6 @@ class WebhookController < ApplicationController
       when Line::Bot::Event::Message
         begin
           Line::SaveReceivedMessage.new(admin, user).call(event) # 受け取ったメッセージをDBに保存
-        rescue ActiveRecord::RecordInvalid => e
-          puts "User creation failed: #{e.message}"
-          Line::SaveSentMessage.new(admin, user).call_with_text(
-            text: "Sorry, User creation failed."
-          )
-          return
         rescue => e
           puts "An unexpected error occurred: #{e.message}"
           Line::SaveSentMessage.new(admin, user).call_with_text(
@@ -60,38 +38,7 @@ class WebhookController < ApplicationController
           )
           return
         end
-        begin
-          # 受け取ったメッセージ内容によって処理を分岐
-          case event["message"]["text"]
-          when "Hello" # 初期セッティング
-            Line::SaveSentMessage.new(admin, user).call_with_text(text: "Hi! Thank you for using me.")
-            Line::SaveSentMessage.new(admin, user).call_with_text(text: "Please click the following URL to authenticate your Google account.\n\n#{auth_url(user)}")
-          when "Get Upcoming Event"
-            events_list = Calendar::GoogleCalendar.new(user).get_events(1)
-            events_list[:summary].each do |value|
-              Line::SaveSentMessage.new(admin, user).call_with_text(text: value)
-            end
-          when "Get 10 Events"
-            end
-            events_list = Calendar::GoogleCalendar.new(user).get_events(10)
-            events_list[:summary].each do |value|
-              Line::SaveSentMessage.new(admin, user).call_with_text( text: value)
-            end
-          when "Switch Between Google Accounts"
-            Line::SaveSentMessage.new(admin, user).call_with_text(text: "Please click the following URL to authenticate your Google account.\n\n#{auth_url(user)}")
-          else
-            Line::SaveSentMessage.new(admin, user).call_with_text(text: "Please enter the correct value.")
-          end
-        rescue => e
-          3.times do
-            puts "An Error occurred!!!"
-          end
-          puts e
-          Line::SaveSentMessage.new(admin, user).call_with_text(
-            text: "Sorry, an error occurred on the server.\nPlease contact the system administrator or click the following URL to authenticate your Google account again.\n\n#{auth_url(user)}"
-          )
-          return
-        end
+        Line::MessageProcesser.new(admin, user, event)
       end
     end
 
@@ -99,8 +46,7 @@ class WebhookController < ApplicationController
   end
 
   private
-  # TODO: _path(user_id: user.id)の形でヘルパーメソッドで渡すようにする
   def auth_url(user)
-    "https://#{ENV['HOST']}/calendar/auth/#{user.id}"
+    auth_google_oauth_index_url(user_id: user.id)
   end
 end
